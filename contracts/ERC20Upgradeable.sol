18b92886d757bb21b3b80a4f3f1b4da5cd5de472
0xharuto <zetazeros@protonmail.com>
2022-08-09 12:43:01 +0100

    added imports

---
+++ contracts/ERC20Upgradeable.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// Adopted from https://github.com/OpenZeppelin/openzeppelin-contracts-upgradeable/blob/v3.4.0/contracts/token/ERC20/ERC20Upgradeable.sol
+
+pragma solidity 0.7.5;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+
