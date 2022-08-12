ac536c6966d7dc9caf67dba7e766f964b4c42435
0xharuto <zetazeros@protonmail.com>
2022-08-12 10:20:29 +0100

    submit data

--- contracts/Oracles.sol
+++ contracts/Oracles.sol
@@ -14,3 +14,41 @@ import "./interfaces/IOracles.sol";
 import "./interfaces/IMerkleDistributor.sol";
 import "./interfaces/IPoolValidators.sol";
 
+/**
+ * @title Oracles
+ *
+ * @dev Oracles contract stores accounts responsible for submitting or update values based on the off-chain data.
+ * The threshold of inputs from different oracles is required to submit the data.
+ */
+
+contract Oracles is IOracles, OwnablePausableUpgradeable {
+    using SafeMathUpgradeable for uint256;
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+
+    bytes32 public constant ORACLE_ROLE = keccak256("ORACLE_ROLE");
+
+    // @dev Rewards nonce is used to protect from submitting the same rewards vote several times.
+    CountersUpgradeable.Counter private rewardsNonce;
+
+    // @dev Validators nonce is used to protect from submitting the same validator vote several times.
+    CountersUpgradeable.Counter private validatorsNonce;
+
+    // @dev Address of the RewardEthToken contract.
+    IRewardEthToken private rewardEthToken;
+
+    // @dev Address of the Pool contract.
+    IPool private pool;
+
+    // @dev Address of the PoolValidators contract.
+    IPoolValidators private poolValidators;
+
+    // @dev Address of the MerkleDistributor contract.
+    IMerkleDistributor private merkleDistributor;
+
+    /**
+    * @dev Modifier for checking whether the caller is an oracle.
+    */
+    modifier onlyOracle() {
+        require(hasRole(ORACLE_ROLE, msg.sender), "Oracles: access denied");
+        _;
+    }
